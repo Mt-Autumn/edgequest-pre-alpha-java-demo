3f7b78f8451858177d3c5cb29e82ac3df6bb7a4f
kevin <kevin@n2-145-53.dhcp.drexel.edu>
2016-09-29 12:14:43 -0400

    Moves visible blocks calculation to renderer manager

--- src/com/mtautumn/edgequest/TerrainManager.java
+++ src/com/mtautumn/edgequest/TerrainManager.java
@@ -12,18 +12,11 @@ public class TerrainManager extends Thread {
 		while (true) {
 			try {
 				if (!sceneManager.system.isGameOnLaunchScreen) {
-					double tileWidth = Double.valueOf(sceneManager.settings.screenWidth) / sceneManager.settings.blockSize / 2.0 + 1;
-					double tileHeight = Double.valueOf(sceneManager.settings.screenHeight) / sceneManager.settings.blockSize / 2.0 + 1;
 					if (sceneManager.system.blockGenerationLastTick || sceneManager.system.characterMoving) {
 						blocksPerTick = 0;
-						sceneManager.system.minTileX = (int) (sceneManager.savable.charX - tileWidth - 1);
-						sceneManager.system.maxTileX = (int) (sceneManager.savable.charX + tileWidth);
-						sceneManager.system.minTileY = (int) (sceneManager.savable.charY - tileHeight - 1);
-						sceneManager.system.maxTileY = (int) (sceneManager.savable.charY + tileHeight);
-
-						for(int i = sceneManager.system.minTileX - 2; i <= sceneManager.system.maxTileX + 1; i++) {
+						for(int i = sceneManager.system.minTileX - 2; i <= sceneManager.system.maxTileX + 1 && blocksPerTick < 1000; i++) {
 							for (int j = sceneManager.system.minTileY - 2; j <= sceneManager.system.maxTileY + 1; j++) {
-								if (!sceneManager.savable.map.containsKey(i + "," + j) && blocksPerTick < 1000) {
+								if (!sceneManager.savable.map.containsKey(i + "," + j)) {
 									terrainGenerator.generateBlock(i, j);
 									blocksPerTick++;
 								}
