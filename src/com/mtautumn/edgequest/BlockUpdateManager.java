fcb23df55164a30e1369bdd8559457d4c459d891
kevin <kevin@10.0.1.4>
2016-10-06 10:58:18 -0400

    Antialiased lighting method

--- src/com/mtautumn/edgequest/BlockUpdateManager.java
+++ src/com/mtautumn/edgequest/BlockUpdateManager.java
@@ -1,5 +1,8 @@
 package com.mtautumn.edgequest;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.mtautumn.edgequest.data.DataManager;
 
 public class BlockUpdateManager extends Thread {
@@ -14,13 +17,43 @@ public class BlockUpdateManager extends Thread {
 				updateBlockLighting(i, j);
 			}
 		}
+		antialiasLighting(x, y);
+	}
+	private void antialiasLighting(int x, int y) {
+		Map<String, Double> tempMap = new HashMap<String, Double>();
+		for (int i = x - lightDiffuseDistance; i <= x + lightDiffuseDistance; i++) {
+			for (int j = y - lightDiffuseDistance; j <= y + lightDiffuseDistance; j++) {
+				tempMap.put(i + "," + j, getAveragedLighting(i,j));
+			}
+		}
+		for (int i = x - lightDiffuseDistance; i <= x + lightDiffuseDistance; i++) {
+			for (int j = y - lightDiffuseDistance; j <= y + lightDiffuseDistance; j++) {
+				setBrightness(i,j,(tempMap.get(i + "," + j) + getBlockBrightness(i,j))/2.0);
+			}
+		}
+	}
+	private double getAveragedLighting(int x, int y) {
+		double lighting = 0.0;
+		for (int i = x - 1; i <= x + 1; i++) {
+			for (int j = y - 1; j <= y + 1; j++) {
+				lighting += getBlockBrightness(i,j);
+			}
+		}
+		return lighting / 9.0;
+	}
+	private double getBlockBrightness(int x, int y) {
+		if (dataManager.savable.lightMap.containsKey(x + "," + y)) {
+			return Double.valueOf(((int) dataManager.savable.lightMap.get(x + "," + y) + 128)) / 255.0;
+		} else {
+			return 0.0;
+		}
 	}
 	private void updateBlockLighting(int x, int y) {
 		double brightness = 0.0;
 		for (int i = x - lightDiffuseDistance; i <= x + lightDiffuseDistance; i++) {
 			for (int j = y - lightDiffuseDistance; j <= y + lightDiffuseDistance; j++) {
 				if (doesContainLightSource(i,j)) {
-					if (isLineOfSight(x,y,i,j)) {
+					if (isLineOfSight(i,j,x,y)) {
 						double distance = Math.sqrt(Math.pow(x-i, 2) + Math.pow(y-j, 2));
 						double brightnessAdded = 1.0 - distance/Double.valueOf(lightDiffuseDistance);
 						if (brightnessAdded < 0) { brightnessAdded = 0; }
@@ -39,15 +72,15 @@ public class BlockUpdateManager extends Thread {
 		return false;
 	}
 	private boolean isLineOfSight(int x1, int y1, int x2, int y2) {
-		double checkingPosX = x1 + 0.5;
-		double checkingPosY = y1 + 0.5;
+		double checkingPosX = Double.valueOf(x1) + 0.5;
+		double checkingPosY = Double.valueOf(y1) + 0.5;
 		double deltaX = (x2-x1);
 		double deltaY = (y2-y1);
 		boolean answer = true;
 		if (deltaX != 0 || deltaY != 0) {
 			while(isInBetween(x1 + 0.5,x2 + 0.5,checkingPosX) && isInBetween(y1 + 0.5,y2 + 0.5,checkingPosY)) {
 				if (isBlockOpaque((int)Math.floor(checkingPosX), (int)Math.floor(checkingPosY))) {
-					if (x1 != Math.floor(checkingPosX) || y1 != Math.floor(checkingPosY)) {
+					if (x2 != Math.floor(checkingPosX) || y2 != Math.floor(checkingPosY)) {
 						answer = false;
 					}
 				}
