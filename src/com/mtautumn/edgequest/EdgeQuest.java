1c5e337430c98d9fdb5d884848dad5aeba6763c2
kevin <kevin@10.0.1.4>
2016-10-08 15:46:21 -0400

    Improves option pane text on windows

--- src/com/mtautumn/edgequest/EdgeQuest.java
+++ src/com/mtautumn/edgequest/EdgeQuest.java
@@ -5,6 +5,22 @@ import com.mtautumn.edgequest.data.DataManager;
 public class EdgeQuest {
 	public static DataManager dataManager = new DataManager();
 	public static void main(String[] args) throws InterruptedException {
+		byte os = (byte) System.getProperty("os.name").toLowerCase().charAt(0);
+		switch (os) {
+		case 108:
+			System.out.println("Setting OS to GNU/Linux");
+			dataManager.system.os = 0;
+			break;
+		case 109:
+			System.out.println("Setting OS to macOS");
+			dataManager.system.os = 1;
+			break;
+		case 119:
+			System.out.println("Setting OS to Windows");
+			dataManager.system.os = 2;
+		default:
+			break;
+		}
 		dataManager.characterManager.start();
 		dataManager.terrainManager.start();
 		dataManager.rendererManager.start();
