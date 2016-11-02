09fa5bf9ea6877867f910278f6bd9ae81f79a298
Gray <graymury@gmail.com>
2016-11-02 16:52:57 -0400

    Add chaos variable to drunkards walk. Add new walk types

--- src/com/mtautumn/edgequest/generator/DrunkardsWalk.java
+++ src/com/mtautumn/edgequest/generator/DrunkardsWalk.java
@@ -20,7 +20,35 @@ public class DrunkardsWalk {
 		this.rng = new Random(seed);
 	}
 	
-	public int[][] walk(int[][] map, int passes) {
+	// Walk types
+	
+	public int[][] shortRandomWalk(int[][] map) {
+		return walk(map, shortWalkPasses, 0.0f);
+	}
+	
+	public int[][] longRandomWalk(int[][] map) {
+		return walk(map, longWalkPasses, 0.0f);
+	}
+	
+	public int[][] shortSemiDrunkWalk(int[][] map) {
+		return walk(map, shortWalkPasses, 0.5f);
+	}
+	
+	public int[][] longSemiDrunkWalk(int[][] map) {
+		return walk(map, longWalkPasses, 0.5f);
+	}
+	
+	public int[][] shortAntWalk(int[][] map) {
+		return walk(map, shortWalkPasses, 0.75f);
+	}
+	
+	public int[][] longAntWalk(int[][] map) {
+		return walk(map, longWalkPasses, 0.75f);
+	}
+	
+	
+	// Does most of the heavy lifting
+	public int[][] walk(int[][] map, int passes, float chaosChance) {
 		
 		int xMax = map.length;
 		int yMax = map[0].length;
@@ -29,11 +57,19 @@ public class DrunkardsWalk {
 		int x = this.rng.nextInt(xMax);
 		int y = this.rng.nextInt(yMax);
 		
+		int[] oldDir = {0, 0};
+		
 		for (int i = 0; i < passes; i++) {
 			
 			if (map[x][y] == 0) { map[x][y] = 1; }
 			
-			int[] dir = changeDirection();
+			int[] dir = changeDirection(chaosChance);
+			if (dir[0] == 0 && dir[1] == 0) {
+				dir = oldDir;
+			} else {
+				oldDir = dir;
+			}
+			
 			x += dir[0];
 			y += dir[1];
 			
@@ -45,33 +81,30 @@ public class DrunkardsWalk {
 		
 	}
 	
-	public int[][] shortWalk(int[][] map) {
-		
-		return walk(map, shortWalkPasses);
-		
-	}
-	
-	public int[][] longWalk(int[][] map) {
-		return walk(map, longWalkPasses);
-	}
-	
-	private int[] changeDirection() {
+	// Direction changer
+	// I mean... it's technically a cellular autonoma
+	private int[] changeDirection(float chaosChance) {
 		
 		int[] i = new int[2];
 		int r = this.rng.nextInt(4);
 		
-		if (r == 0) {
+		if (this.rng.nextFloat() < chaosChance) {
 			i[0] = 0;
-			i[1] = 1;
-		} else if (r == 1) {
-			i[0] = 0;
-			i[1] = -1;
-		} else if (r == 2 ) {
-			i[0] = -1;
 			i[1] = 0;
 		} else {
-			i[0] = 1;
-			i[1] = 0;
+			if (r == 0) {
+				i[0] = 0;
+				i[1] = 1;
+			} else if (r == 1) {
+				i[0] = 0;
+				i[1] = -1;
+			} else if (r == 2 ) {
+				i[0] = -1;
+				i[1] = 0;
+			} else {
+				i[0] = 1;
+				i[1] = 0;
+			}
 		}
 		
 		return i;
