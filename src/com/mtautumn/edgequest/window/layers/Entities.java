531a5848a6f7852d5c35f36486caf125abde296b
keco185 <keco185@me.com>
2016-11-02 14:41:15 -0400

    Fixes character jitter

--- src/com/mtautumn/edgequest/window/layers/Entities.java
+++ src/com/mtautumn/edgequest/window/layers/Entities.java
@@ -10,14 +10,14 @@ public class Entities {
 		Color.white.bind();
 		for( int i = 0; i < r.dataManager.savable.entities.size(); i++) {
 			Entity entity = r.dataManager.savable.entities.get(i);
-			drawEntity(entity.getTextureName(), entity.getX(), entity.getY(), entity.getRot(), r);
+			drawEntity(entity.getTextureName(), entity.frameX, entity.frameY, entity.getRot(), r);
 		}
 	}
 	private static void drawEntity(String texture, double posX, double posY, byte rotation, Renderer r) {
 		double blockSize = r.dataManager.settings.blockSize;
-		int pixelsX = (int) ((posX - (r.dataManager.system.screenX - (Double.valueOf(r.dataManager.settings.screenWidth)/2.0)/blockSize))*blockSize);
-		int pixelsY = (int) ((posY - (r.dataManager.system.screenY - (Double.valueOf(r.dataManager.settings.screenHeight)/2.0)/blockSize))*blockSize);
-		r.drawTexture(r.textureManager.getTexture(texture), (int) (pixelsX - blockSize / 2.0), (int) (pixelsY - blockSize / 2.0), (int) blockSize, (int) blockSize, Float.valueOf(rotation) * 45.0f);
+		double pixelsX = (float) ((posX - (r.dataManager.system.screenX - (Double.valueOf(r.dataManager.settings.screenWidth)/2.0)/blockSize))*blockSize);
+		double pixelsY = (float) ((posY - (r.dataManager.system.screenY - (Double.valueOf(r.dataManager.settings.screenHeight)/2.0)/blockSize))*blockSize);
+		r.drawTexture(r.textureManager.getTexture(texture), (float) (pixelsX - blockSize / 2.0), (float) (pixelsY - blockSize / 2.0), (float) blockSize, (float) blockSize, Float.valueOf(rotation) * 45.0f);
 
 	}
 }
